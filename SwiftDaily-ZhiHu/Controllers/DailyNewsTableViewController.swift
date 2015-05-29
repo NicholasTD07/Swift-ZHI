4bd9cb46b00ad6dbaa369007711cd46e9b8bea6e
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-05-29 19:18:09 +1000

    Show latest daily news

---
+++ SwiftDaily-ZhiHu/Controllers/DailyNewsTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  DailyNewsTableViewController.swift
+//  SwiftDaily-ZhiHu
+//
+//  Created by Nicholas Tian on 29/05/2015.
+//  Copyright (c) 2015 nickTD. All rights reserved.
+//
+
+import UIKit
+import SwiftDailyAPI
+
+class DailyNewsTableViewController: UITableViewController {
+    private let api = DailyAPI()
+    private var dailyNewsMeta = [NewsMeta]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        api.latestDaily { latestDaily in
+            if let latestDaily = latestDaily {
+                println("got back daily news at date: " + latestDaily.date.description)
+                self.dailyNewsMeta.extend(latestDaily.news)
+                self.tableView.reloadData()
+            }
+        }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return dailyNewsMeta.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NewsMetaCell", forIndexPath: indexPath) as! UITableViewCell
+
+        let newsMeta = dailyNewsMeta[indexPath.row]
+        cell.textLabel?.text = newsMeta.title
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
