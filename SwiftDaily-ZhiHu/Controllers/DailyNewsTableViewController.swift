0df58f8bc570845d16a11c7d6dd994340ad83a24
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-12 21:52:54 +1000

    Remove template code by Xcode

--- SwiftDaily-ZhiHu/Controllers/DailyNewsTableViewController.swift
+++ SwiftDaily-ZhiHu/Controllers/DailyNewsTableViewController.swift
@@ -70,16 +70,4 @@ extension DailyTableViewController {
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return true
     }
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
 }
