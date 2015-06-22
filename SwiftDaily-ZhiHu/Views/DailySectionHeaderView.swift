8e10acad8b660d9b873ac56c506e5f0287295f0c
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-22 20:44:48 +1000

    Add `DailySectionHeaderView`

---
+++ SwiftDaily-ZhiHu/Views/DailySectionHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  DailySectionHeaderView.swift
+//  SwiftDaily-ZhiHu
+//
+//  Created by Nicholas Tian on 22/06/2015.
+//  Copyright © 2015 nickTD. All rights reserved.
+//
+
+import UIKit
+
+class DailySectionHeaderView: UITableViewHeaderFooterView {
+    @IBOutlet weak var titleLabel: UILabel!
+}
