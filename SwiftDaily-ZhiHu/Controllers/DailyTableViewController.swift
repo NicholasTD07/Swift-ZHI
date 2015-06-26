5cf8171b9996487ec3a88d80aaac1b906cb366ec
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-26 19:54:38 +1000

    Extract common parts into `DailyTableViewController`
    Highlight #34
    Don't like to repeat myself. DRY

---
+++ SwiftDaily-ZhiHu/Controllers/DailyTableViewController.swift
@@ -0,0 +1,80 @@
+//
+//  DailyTableViewController.swift
+//  SwiftDaily-ZhiHu
+//
+//  Created by Nicholas Tian on 26/06/2015.
+//  Copyright © 2015 nickTD. All rights reserved.
+//
+
+import UIKit
+
+class DailyTableViewController: HidesHairLineUnderNavBarViewController {
+
+    func hasNewsMetaAtIndexPath(indexPath: NSIndexPath) -> Bool {
+        fatalError()
+    }
+
+    func dateStringAtSection(section: Int) -> String {
+        fatalError()
+    }
+
+    // MARK: UI
+    @IBOutlet weak var tableView: UITableView!
+    let refreshControl: UIRefreshControl = UIRefreshControl()
+
+    var firstAppeared = false
+    let dateFormatter: NSDateFormatter = {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        return dateFormatter
+        }()
+}
+
+// MARK: UI methods
+extension DailyTableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUi()
+    }
+
+    func setupUi() {
+        tableView.registerNib(UINib(nibName: "DailySectionHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "DailySectionHeaderView")
+
+        refreshControl.addTarget(self, action: "refreshLatestDaily", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
+    }
+
+    func cellAtIndexPath(indexPath: NSIndexPath) -> UITableViewCell {
+        fatalError()
+    }
+}
+
+extension DailyTableViewController {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        guard hasNewsMetaAtIndexPath(indexPath) else {
+            let loadingCell = tableView.dequeueReusableCellWithIdentifier("LoadingCell", forIndexPath: indexPath) as! LoadingCell
+            loadingCell.activityIndicator.startAnimating()
+            return loadingCell
+        }
+
+        return cellAtIndexPath(indexPath)
+    }
+
+
+    // TODO: clean up
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier("DailySectionHeaderView") as? DailySectionHeaderView else { return nil }
+
+        header.backgroundView = {
+            let view = UIView(frame: header.bounds)
+            // HACK: To put color in Storyboard, not in code
+            view.backgroundColor = header.titleLabel.highlightedTextColor
+            return view
+            }()
+
+        header.titleLabel.text = self.dateStringAtSection(section)
+
+        return header
+    }
+}
--- SwiftDaily-ZhiHu/Controllers/RealmDailyTableViewController.swift
+++ SwiftDaily-ZhiHu/Controllers/RealmDailyTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import RealmSwift
 import SwiftDailyAPI
 
-class RealmDailyTableViewController: HidesHairLineUnderNavBarViewController {
+class RealmDailyTableViewController: DailyTableViewController {
     private let store = DailyRealmStore()
     private let dailyDates = DailyDates()
 
@@ -30,22 +30,27 @@ class RealmDailyTableViewController: HidesHairLineUnderNavBarViewController {
         return daily.news[indexPath.row]
     }
 
-    // MARK: UI
-    private var firstAppeared = false
-    @IBOutlet weak var tableView: UITableView!
+    override func hasNewsMetaAtIndexPath(indexPath: NSIndexPath) -> Bool {
+        return newsMetaAtIndexPath(indexPath) != nil
+    }
 
-    // TODO: clean up
-    private let dateFormatter: NSDateFormatter = {
-        let dateFormatter = NSDateFormatter()
-        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
-        return dateFormatter
-        }()
-    private let refreshControl: UIRefreshControl = UIRefreshControl()
-
+    override func dateStringAtSection(section: Int) -> String {
+        let date = dailyDates.dateAtIndex(section)
+        return dateFormatter.stringFromDate(date)
+    }
 }
+
 
 // MARK: UI methods
 extension RealmDailyTableViewController {
+    override func cellAtIndexPath(indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NewsMetaCell", forIndexPath: indexPath)
+        let newsMeta = newsMetaAtIndexPath(indexPath)!
+
+        cell.textLabel?.text = newsMeta.title
+
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,18 +59,7 @@ extension RealmDailyTableViewController {
             self.dailyDates.endDate = self.store.latestDate
             self.tableView.reloadData()
         }
-
-        setupUi()
     }
-
-    // TODO: clean up
-    private func setupUi() {
-        tableView.registerNib(UINib(nibName: "DailySectionHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "DailySectionHeaderView")
-
-        refreshControl.addTarget(self, action: "refreshLatestDaily", forControlEvents: UIControlEvents.ValueChanged)
-        tableView.addSubview(refreshControl)
-    }
-
 
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -88,38 +82,5 @@ extension RealmDailyTableViewController: UITableViewDataSource {
 
         return daily.news.count
     }
-
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        guard let newsMeta = newsMetaAtIndexPath(indexPath) else {
-            let loadingCell = tableView.dequeueReusableCellWithIdentifier("LoadingCell", forIndexPath: indexPath) as! LoadingCell
-            loadingCell.activityIndicator.startAnimating()
-            return loadingCell
-        }
-
-        // TODO: configueCell
-        let cell = tableView.dequeueReusableCellWithIdentifier("NewsMetaCell", forIndexPath: indexPath)
-
-        cell.textLabel?.text = newsMeta.title
-
-        return cell
-    }
-
-    // TODO: clean up
-    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let header = tableView.dequeueReusableHeaderFooterViewWithIdentifier("DailySectionHeaderView") as? DailySectionHeaderView else { return nil }
-
-        header.backgroundView = {
-            let view = UIView(frame: header.bounds)
-            // HACK: To put color in Storyboard, not in code
-            view.backgroundColor = header.titleLabel.highlightedTextColor
-            return view
-            }()
-        header.titleLabel.text = dateFormatter.stringFromDate(dailyDates.dateAtIndex(section))
-
-        return header
-    }
-}
-
-extension RealmDailyTableViewController: UITableViewDelegate {
 
 }
