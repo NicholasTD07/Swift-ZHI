e9a498dc4d170f8be4304b0b3e9e2811aedbc791
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-26 17:37:22 +1000

    Add `DailyDates` [WIP]

---
+++ SwiftDaily-ZhiHu/Models/DailyDates.swift
@@ -0,0 +1,13 @@
+//
+//  DailyDates.swift
+//  SwiftDaily-ZhiHu
+//
+//  Created by Nicholas Tian on 26/06/2015.
+//  Copyright © 2015 nickTD. All rights reserved.
+//
+
+import Foundation
+
+class DailyDates {
+    static let startDate = NSDate.dateAt(year: 2013, month: 05, day: 19)!
+}
