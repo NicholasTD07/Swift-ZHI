cad7eee1f5aa28b1b18c3e829958f2491a326da5
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-06-26 18:45:07 +1000

    Add `dateAtIndex` and `days` in `DailyDates`

--- SwiftDaily-ZhiHu/Helpers/DailyDates.swift
+++ SwiftDaily-ZhiHu/Helpers/DailyDates.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class DailyDates {
-    static let startDate = NSDate.dateAt(year: 2013, month: 05, day: 19)!
+    let startDate = NSDate.dateAt(year: 2013, month: 05, day: 19)!
+    var endDate: NSDate
+
+    private var calendar = NSCalendar.currentCalendar()
+
+    init(endDate: NSDate = NSDate()) {
+        self.endDate = endDate
+    }
+
+    func dateAtIndex(index: Int) -> NSDate {
+        return calendar.dateByAddingUnit(.Day, value: -index, toDate: endDate, options: [])!
+    }
+
+    func days() -> Int {
+        return calendar.components(.Day, fromDate: startDate, toDate: endDate, options: []).day
+    }
+
 }
