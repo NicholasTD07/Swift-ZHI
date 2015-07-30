5f64981c4d36ecd4e84a466149b0fcde8978855a
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-07-30 23:15:43 +1000

    CommentSectionHeaderView: view transparency, label constants, #11

--- SwiftDaily-ZhiHu/Controllers/RealmCommentViewController.swift
+++ SwiftDaily-ZhiHu/Controllers/RealmCommentViewController.swift
@@ -116,7 +116,7 @@ extension RealmCommentViewController: UITableViewDelegate {
             header.backgroundView = {
                 let view = UIView(frame: header.bounds)
                 view.backgroundColor = tableView.backgroundColor
-                view.alpha = 0.95
+                view.alpha = 0.96
                 return view
             }()
 
