7fb7c32ff034be9063e84895b1a6c9a9379a1a89
NicholasTD07 <Nicholas.TD07@gmail.com>
2015-08-03 10:32:52 +1000

    Split `DailyRealmStore` into smaller extensions

--- SwiftDaily-ZhiHu/Models/DailyRealmStore.swift
+++ SwiftDaily-ZhiHu/Models/DailyRealmStore.swift
@@ -23,23 +23,10 @@ public class DailyRealmStore {
     {
         dailyAPI = DailyAPI(completionQueue: completionQueue)
     }
+}
 
-    public func dailyAtDate(date: NSDate) -> DailyObject? {
-        let results = realm.objects(DailyObject).filter("dateHash == \(date.hash)")
-        return results.first
-    }
-
-    public func newsWithId(newsId: Int) -> NewsObject? {
-        let results = realm.objects(NewsObject).filter("newsId == \(newsId)")
-        return results.first
-    }
-
-    public func deleteNewsWithId(newsId: Int, inRealm realm: Realm = defaultRealm()) {
-        if let news = newsWithId(newsId) {
-            realm.write { realm.delete(news) }
-        }
-    }
-
+extension DailyRealmStore {
+    // MARK: Insertion by calling to `DailyAPI`
     public func updateLatestDaily() {
         dailyAPI.latestDaily { latestDaily in
             self.latestDaily = latestDaily
@@ -51,15 +38,38 @@ public class DailyRealmStore {
         dailyAPI.daily(forDate: date) { self.addDaily($0) }
     }
 
+    public func news(newsId: Int) {
+        dailyAPI.news(newsId) { self.addObject(NewsObject.from($0)) }
+    }
+
+    // MARK: Deletion
+    public func deleteNewsWithId(newsId: Int, inRealm realm: Realm = defaultRealm()) {
+        if let news = newsWithId(newsId) {
+            realm.write { realm.delete(news) }
+        }
+    }
+}
+
+// MARK: Getters
+extension DailyRealmStore {
+    public func dailyAtDate(date: NSDate) -> DailyObject? {
+        let results = realm.objects(DailyObject).filter("dateHash == \(date.hash)")
+        return results.first
+    }
+
+    public func newsWithId(newsId: Int) -> NewsObject? {
+        let results = realm.objects(NewsObject).filter("newsId == \(newsId)")
+        return results.first
+    }
+}
+
+// MARK: Private methods
+extension DailyRealmStore {
     private func addDaily(daily: Daily, toRealm realm: Realm = defaultRealm()) {
         addObject(DailyObject.from(daily))
     }
 
     private func addObject(object: Object, toRealm realm: Realm = defaultRealm()) {
         realm.write { realm.add(object, update: true) }
-    }
-
-    public func news(newsId: Int) {
-        dailyAPI.news(newsId) { self.addObject(NewsObject.from($0)) }
     }
 }
